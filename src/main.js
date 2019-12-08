9a4bde99191eea4655067542aa9c898daa79e72b
Mo Surface <zhaimo@outlook.com>
2019-12-08 13:43:40 -0600

    added axios read data from json

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import axios from 'axios'
+import VueAxios from 'vue-axios' 
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  axios,
+  VueAxios,
   vuetify,
   render: h => h(App)
 }).$mount("#app");
